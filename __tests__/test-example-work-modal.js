6e217a678463ba9c83e15906d7876d042a44ec23
Shivali Singh <shivali.singh2000@gmail.com>
2020-08-01 15:10:40 -0400

    update test

---
+++ __tests__/test-example-work-modal.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWorkModal from '../js/example-work-modal';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const myExample = {
+  'title': "Blog on AWS Certifications",
+  'href': "https://about.shivali.us",
+  'desc': " testing",
+  'image': {
+    'desc': "example screenshot of a project involving code",
+    'src': "images/example1.png",
+    'comment': ""
+  }
+};
+
+describe("ExampleWorkModal Component", () => {
+  let component = shallow(<ExampleWorkModal example={myExample}
+    open={false}/>);
+  let openComponent = shallow(<ExampleWorkModal example={myExample}
+    open={true}/>);
+
+  let anchors = component.find('a');
+
+  it("Should contain a single anchor tag", () => {
+    expect(anchors.length).toEqual(1);
+  });
+
+  it("Should link to our project", () => {
+    expect(anchors.prop('href')).toEqual(myExample.href);
+  });
+
+  it("Should have the modal class set correctly", () => {
+    expect(component.find(".background--skyBlue").hasClass("modal--closed")).toBe(true);
+    expect(openComponent.find(".background--skyBlue").hasClass("modal--open")).toBe(true);
+  })
+})
