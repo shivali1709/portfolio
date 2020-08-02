bff67e5a2935f5ff7aade96ca1a2d19fd4b3ad6d
Shivali Singh <shivali.singh2000@gmail.com>
2020-08-02 11:17:37 -0400

    update

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ import ExampleWork from './example-work.js';
 
 const myWork = [
   {
-    'title': "Blog on AWS Certifications",
+    'title': "Blog post on being 3 x AWS Certified",
     'href': "https://www.linkedin.com/pulse/becoming-3x-aws-certified-beginners-guide-cloud-shivali-singh/",
-    'desc': "If one thing is certain today, it’s never too late to cash in on the cloud. You can deploy technology services in a matter of minutes, and get from idea to implementation several orders of magnitude faster than before. This gives you the freedom to experiment, test new ideas to differentiate customer experiences and transform your business.",
+    'desc': "If one thing is certain today, it’s never too late to cash in on the cloud. You can deploy technology services in a matter of minutes, and get from idea to implementation several orders of magnitude faster than before. This gives you the freedom to experiment, test new ideas to differentiate customer experiences and transform your business. Check out my article on becoming 3 x AWS Certified here.",
     'image': {
       'desc': "example screenshot of a project involving code",
       'src': "images/example1.png",
@@ -24,9 +24,9 @@ const myWork = [
     }
   },
   {
-    'title': "My Cloud Resume",
+    'title': "A Serverless Portfolio",
     'href': "https://github.com/shivali1709/portfolio",
-    'desc': " test3",
+    'desc': "A personal project to create a Serverless Portfolio website using ReactJS on AWS Serverless along with fully automated CI/CD process.",
     'image': {
       'desc': "example screenshot of a project involving cats",
       'src': "images/example2.png",
